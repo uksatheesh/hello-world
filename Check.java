0bf62eb67f3616481bc2c0bd92ac86dcfccd6a46
Satheesh <uksatheesh@gmail.com>
2020-05-03 22:45:37 +0800

    Create Check.java
    ASASDD

---
+++ Check.java
@@ -0,0 +1,18 @@
+package org.test;
+
+public class Check {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		String s=new String("new");
+		//s+="hi";
+		System.out.println("value="+s);
+		String s1="new";
+		System.out.print( s1.equals(s));
+		
+	}
+	
+}
